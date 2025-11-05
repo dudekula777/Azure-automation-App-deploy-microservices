87394cee48c81ff4e69172b188e6955848a0c980
dudekula777 <dudekula777@gmail.com>
2025-11-05 18:48:08 +0530

    updated

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = var.aks_resource_group
-    storage_account_name = var.storage_account_name
-    container_name       = "tfstate"
-    key                  = "dev.terraform.tfstate"
+    # resource_group_name  = var.aks_resource_group
+    # storage_account_name = var.storage_account_name
+    # container_name       = "tfstate"
+    # key                  = "dev.terraform.tfstate"
   }
 }
