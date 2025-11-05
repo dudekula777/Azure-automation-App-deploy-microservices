b37417538978eedf5879f89fdd91ba50da9e94ad
dudekula777 <dudekula777@gmail.com>
2025-11-05 22:34:37 +0530

    dev

--- infrastructure/environments/dev/backend.tf
+++ infrastructure/environments/dev/backend.tf
@@ -1,8 +1,36 @@
 terraform {
+  required_version = ">= 1.0"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.0"
+    }
+  }
+
   backend "azurerm" {
     resource_group_name  = "tfstate-rg-dev"
     storage_account_name = "tfstatestorage202020"
     container_name       = "tfstate"
     key                  = "dev.terraform.tfstate"
   }
+}
+
+provider "azurerm" {
+  features {}  # Only one features block!
+  
+  # These are optional since you're using environment variables
+  # You can remove these lines entirely if using env vars
+  subscription_id = var.subscription_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id 
+}
+
+provider "random" {
+  # No configuration needed
 }
--- infrastructure/environments/dev/variables.tf
+++ infrastructure/environments/dev/variables.tf
@@ -21,3 +21,28 @@ variable "default_node_count" {
   type        = number
   default     = 2
 }
+
+variable "subscription_id" {
+  description = "Azure subscription ID"
+  type        = string
+  default     = ""  # Optional if using env vars
+}
+
+variable "tenant_id" {
+  description = "Azure tenant ID"
+  type        = string
+  default     = ""  # Optional if using env vars
+}
+
+variable "client_id" {
+  description = "Azure client ID"
+  type        = string
+  default     = ""  # Optional if using env vars
+}
+
+variable "client_secret" {
+  description = "Azure client secret"
+  type        = string
+  sensitive   = true
+  default     = ""  # Optional if using env vars
+}
