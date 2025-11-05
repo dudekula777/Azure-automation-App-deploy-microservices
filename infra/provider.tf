4a677b79540f51f953ed2fc2d819d233339a601d
dudekula777 <dudekula777@gmail.com>
2025-11-05 16:39:53 +0530

    updated

--- infra/provider.tf
+++ infra/provider.tf
@@ -1,10 +1,9 @@
 provider "azurerm" {
-  features {}
-
+  features = {}
+  tenant_id       = var.tenant_id
+  subscription_id = var.subscription_id
   client_id       = var.client_id
   client_secret   = var.client_secret
-  subscription_id = var.subscription_id
-  tenant_id       = var.tenant_id
 }
 
 provider "azuread" {
